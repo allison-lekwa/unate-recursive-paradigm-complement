13e82816bd130928c992d123c65ff9e3648c3ae5
Allison <allison2spartamath@gmail.com>
2023-07-30 10:53:24 -0700

    validate input function

--- unate-recursive-complement-calc.py
+++ unate-recursive-complement-calc.py
@@ -18,55 +18,6 @@ def File_To_List_Converter (input_file):
     print(func)
     return num_of_vars, num_of_cubes, func
 
-# this was not stipulated, so you can leave it out
-def ValidateFunc(num_of_cubes, func):
-    if num_of_cubes != len(func):
-        return False, 'invalid function!!! Cubelist must be equal'\
-            ' to number of individual expressions'
-    else:
-        for i in range(0, len(func)):
-            if func[i][0] != len(func[i]) - 1:
-                return False, 'Invalid!!! number of dont cares do not equal '\
-                        'to number of variables for' + func[i] + 'check line' + i
-        return True, 'valid'
-
-def Check_Cube_List_With_Dont_Cares(num_of_cubes, func):
-    dont_care_flag = False # set initial state of number of dont care cube list to false
-    if len(func) != num_of_cubes: #number of cube lists must equal to number of functions
-        return not(dont_care_flag)
-    for i in range(0, len(func)):
-        if func[i][0] == 0 and len(func[i]) == 1: #check if the number of "not dont care" is zero
-            return not(dont_care_flag)
-    return dont_care_flag
-
-def Complement(num_of_vars, num_of_cubes, func):
-    print(len(func))
-    if len(func) == 0: # function has empty cube list
-        return [[ '11' for i in range(0, num_of_vars)]] # Complement zeros
-    
-    dont_care_flag = Check_Cube_List_With_Dont_Cares(num_of_cubes, func)
-    if dont_care_flag: #function has at least a cube with dont cares
-        return [[ '00' for i in range(0, num_of_vars)]] # Complement dont cares i.e '1'
-    else:
-        return
-    
-def Output_To_File(vars_in, func_complement):
-    now = datetime.now() # current date and time
-    fh=open("Output"+now,"w")
-    fh.write(str(vars_in)+"\n")
-    fh.write(str(len(func_complement))+"\n")
-    for x in func_complement:
-        y=[i for i in x if i != "11"]
-        fh.write(str(len(y))+" ")
-        for i in range(0,len(y)):
-            if y[i] == "01":
-                fh.write(str(i+1))
-            elif y[i] == "10":
-                fh.write(str(-(i+1)))
-            if i != len(y)-1:
-                fh.write(" ")
-        fh.write("\n")  
-    fh.close()
 
 def main ():
     print('----------------***************************************----------------')
@@ -79,12 +30,6 @@ def main ():
     input_file = open(file_path, 'r')
     
     num_of_vars, num_of_cubes, func = File_To_List_Converter(input_file)
-    isValid, reason = ValidateFunc(num_of_cubes, func)
-    if isValid:
-        func_complement = Complement(num_of_vars, num_of_cubes, func)
-        
-    Output_To_File(num_of_vars, func_complement)
-    print('')
     
 
 main()
